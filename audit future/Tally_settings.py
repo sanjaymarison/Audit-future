ebf2e7e3b68311b2b759513d68b2eecac3ab38c7
sanjay marison <61217970+sanjaymarison@users.noreply.github.com>
2020-10-03 23:14:57 +0530

    updated settings
    Added new settings features. However it might be buggy.

--- audit future/Tally_settings.py
+++ audit future/Tally_settings.py
@@ -1,15 +1,19 @@
 from tkinter import *
 from styling import *
 from Tally_random_data import random_g
-from tkinter import messagebox
+from tkinter import messagebox,filedialog
 import smtplib
-
+import webbrowser
+import json
+import styling
+import os
 
 def setting():
 
 	settings_window = Tk()
 	settings_window.title("Settings")
-	settings_window.geometry("800x600")
+	settings_window.geometry("800x650")
+	messagebox.showinfo("Note","You have to restart the app for the changes you make here to update")
 
 
 	class information:
@@ -22,9 +26,67 @@ def setting():
 		def _branding_():
 			pass
 		def _export_():
-			pass
+			def choose_location():
+				settings_window.file_choose = filedialog.askdirectory()
+				text_path.delete(0,"end")
+				text_path.insert(0,settings_window.file_choose)
+			def exit_export():
+				window_export.destroy()
+			def save_path_():
+				path_ = str(text_path.get())
+				with open(user_database,"r") as read_file:
+					d = json.load(read_file)
+				d["export-path"] = path_
+				print(d)
+				with open(user_database,"w") as write_file:
+					json.dump(d,write_file)
+				notify_saved = Label(window_export,bg=bg,fg=fg,text="Saved",font=font)
+				notify_saved.grid(row=3,column=0)
+
+			window_export = LabelFrame(panel_options,bg=bg)
+			panel_options.add(window_export)
+
+			path_heading = Label(window_export,bg=bg,fg=fg,text="Enter path to export excel file:",font=font)
+			path_heading.grid(row=0,column=0)
+
+			text_path = Entry(window_export,bg=textbox_bg,fg=textbox_fg,width=30,insertbackground=insert_fg,font=font)
+			text_path.grid(row=1,column=0)
+			text_path.insert(0,export_path)
+
+			save_path = Button(window_export,bg=button_bg,fg=button_fg,text="Save",command=save_path_)
+			save_path.grid(row=2,column=0)
+
+			file_dialog_box = Button(window_export,bg=button_bg,fg=button_fg,font=font,text="Choose location",command=choose_location)
+			file_dialog_box.grid(row=2,column=1)
+
+			exit_button = Button(window_export,fg=button_fg,bg=button_bg,font=font,text="Exit",command=exit_export)
+			exit_button.grid(row=3,column=1)			
+
+
 		def _about_():
-			pass
+			def open_t_c():
+				try:
+					webbrowser.open(tc_path)
+				except:
+					messagebox.showerror("Error","Couldn't find a browser/file")
+			def exit_about():
+				window_about.destroy()
+			about_text = "This app is a product of future lab, run and manged by sanjay marison. Any kind of changes done to this app must be in accordance with the license agreement. Some of the features might not be working and will be fixed in the future updates. Any feedback can be given by choosing the option on the left menu. Note many features are experimental and might not make to the stable release of the app, any of those changes will be notified in the about menu of the app. version : 0.0.1 beta for developers author : Sanjay Marison mail : marisonsanjay@gmail.com"
+			window_about = LabelFrame(panel_options,bg=bg)
+			panel_options.add(window_about)
+
+			about_heading = Label(window_about,bg=bg,fg=fg,text="About",font=(font,30))
+			about_heading.grid(row=0,column=0)
+
+			about_info = Message(window_about,bg=bg,fg=fg,font=(font,24),text=about_text)
+			about_info.grid(row=1,column=0,columnspan=2)
+
+			button_terms = Button(window_about,bg=button_bg,fg=button_fg,text="Terms and conditions",command=open_t_c)
+			button_terms.grid(row=2,column=0)
+
+			exit_button = Button(window_about,fg=button_fg,bg=button_bg,font=font,text="Exit",command=exit_about)
+			exit_button.grid(row=2,column=1)
+
 		def _feedback_():
 			top = LabelFrame(panel_options,bg=bg)
 			panel_options.add(top)
@@ -33,21 +95,21 @@ def setting():
 			label1.grid(row=1)
 
 			gmail_adress_name = Entry(top, width=50)
-			gmail_adress_name.config(fg=fg,bg=bg, insertbackground=fg)
+			gmail_adress_name.config(fg=textbox_fg,bg=textbox_bg, insertbackground=insert_fg)
 			gmail_adress_name.grid(row=2)
 
 			label2 = Label(top, text="Type your mail here:",fg=fg,bg=bg,font=font)
 			label2.grid(row=3)
 
 			gmail_adress_mail = Entry(top, width=50)
-			gmail_adress_mail.config(fg=fg,bg=bg, insertbackground=fg)
+			gmail_adress_mail.config(fg=textbox_fg,bg=textbox_bg, insertbackground=insert_fg)
 			gmail_adress_mail.grid(row=4)
 
 			label3 = Label(top, text="Type your feedback here:",fg=fg,bg=bg,font=font)
 			label3.grid(row=5)
 
 			gmail_adress_body = Text(top)
-			gmail_adress_body.config(fg=fg,bg=bg, insertbackground=fg)
+			gmail_adress_body.config(fg=textbox_fg,bg=textbox_bg, insertbackground=insert_fg)
 			gmail_adress_body.grid(row=6)
 
 			def exit_feedback():
