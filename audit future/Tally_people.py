4587d60850024896f4d7a4563c47961dfc9c33b6
sanjay marison <61217970+sanjaymarison@users.noreply.github.com>
2020-10-03 22:41:14 +0530

    created viewer for people data
    Created a database viewer for people working status data. Commit 1

--- audit future/Tally_people.py
+++ audit future/Tally_people.py
@@ -4,6 +4,7 @@ from Tally_date import date
 from styling import *
 from PIL import Image,ImageTk
 from tkinter import ttk
+from Audit_people_viewer import people_viewer_api
 
 
 def advanced_data(window,window1):
@@ -92,17 +93,22 @@ def advanced_data(window,window1):
 
 
 	button_add_people = Button(window9,text="Add people type",bg=button_bg,fg=button_fg,font=font,command=add_people)
-	button_add_people.grid(row=0,column=1,padx=10,pady=10)
+	button_add_people.grid(row=0,column=2,pady=10)
 
 	reload_button = Button(window9,height=30,width=40,image=img,bg=button_bg,command=main_screen)
 	reload_button.grid(row=0,column=0,padx=10,pady=10,sticky=W)
 
+	view_button = Button(window9,text="View",bg=button_bg,command=people_viewer_api)
+	view_button.grid(row=0,column=1,padx=10,pady=10,sticky=W)
+
 	clicked1 = StringVar()
 	chooose_option1 = ttk.Combobox(window9,textvariable=clicked1)
 	chooose_option1['values'] = type_people
-	chooose_option1.grid(row=2,column=2,padx=10,pady=10)
-
+	chooose_option1.grid(row=2,column=2,pady=10)
+	global i
+	i = 0
 	def add_people_report_to_database():
+		global i
 		conn = sqlite3.connect(DEFAULT_PATH)
 		c = conn.cursor()
 		try:
@@ -113,21 +119,26 @@ def advanced_data(window,window1):
 		
 		conn.commit()
 
-		if given_date == "YYYY/MM/DD":
-			given_date = date
-
 		total_people = int(textbox_for_category_people.get())
 		given_date = str(textbox_date.get())
 		given_people_type = str(clicked1.get())
 
-
+		if given_date == "YYYY/MM/DD":
+			given_date = date
 
 
 		conn.execute('INSERT INTO PEOPLEDATA(date,peoplecount,peopletype) VALUES(?,?,?)',(given_date,total_people,given_people_type,))
 		conn.commit()
 
+		i+=1
+
+		notify_user = Label(window9,bg=button_bg,fg=button_fg,font=font,text=("Added("+str(i)+")"))
+		notify_user.grid(row=4,column=2)
+
 	button_insert_to_db = Button(window9,bg=button_bg,fg=button_fg,font=font,text="ADD",command=add_people_report_to_database)
 	button_insert_to_db.grid(row=3,column=2)
+
+
 
 
 
