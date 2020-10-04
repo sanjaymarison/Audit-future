ba9c8ec248419c1769b92396a21b8ff7e7e8cdb0
sanjay marison <61217970+sanjaymarison@users.noreply.github.com>
2020-10-04 22:56:39 +0530

    Update setup.py

--- audit future/setup.py
+++ audit future/setup.py
@@ -1,4 +1,5 @@
 import os
+from styling import app_path
 required_packages = ["matplotlib","datetime","termcolor","pillow","pyrebase","pyrebase4","xlsxwriter","kivy","pygame"]
 i = 0
 not_installed = []
@@ -11,3 +12,8 @@ for element in required_packages:
 print("No of modules couldn't be installed: ",i)
 print("Modules which couldn't be installed:",not_installed)
 print("This data is not a hundred percent accurate so check the terminal/cmd output for errors and try reinstalling it")
+print("Starting app")
+try:
+	os.system(''.join(("python ",app_path)))
+except:
+	print("Couldn't open file try opening it manually")
