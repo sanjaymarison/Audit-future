d1850155912f72340ea330cca14a8a827f5c380d
sanjay marison <61217970+sanjaymarison@users.noreply.github.com>
2020-10-08 10:02:18 +0530

    Add files via upload

--- audit future/Tally_viewer.py
+++ audit future/Tally_viewer.py
@@ -1,6 +1,5 @@
-from tkinter import Label,Button,Entry,Tk,W,S,E,N,LabelFrame,StringVar,DISABLED
-from tkinter import ttk
-from tkinter import filedialog
+from tkinter import *
+from tkinter import ttk,filedialog
 from styling import *
 import sqlite3
 from Tally_data_arrange import sort_data
@@ -10,8 +9,82 @@ import string
 import time
 from Tally_excel_sheet import export_window
 from Audit_send_record import send_record
+from number import format_num
+from refresh_database import refresh
+from PIL import ImageTk,Image
 
 def viewer_api(date1="Date",name1="Name",category1="Category",payement1="Payement",site1="Site",amount1="Amount",file=DEFAULT_PATH):
+	global img_search
+	img_search = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/search.png")  # PIL solution
+	img_search = img_search.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_search = ImageTk.PhotoImage(img_search)
+
+	global img_add
+	img_add = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/add.png")  # PIL solution
+	img_add = img_add.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_add = ImageTk.PhotoImage(img_add)
+
+	global img_upload
+	img_upload = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/upload.png")  # PIL solution
+	img_upload = img_upload.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_upload = ImageTk.PhotoImage(img_upload)
+
+	global img_info
+	img_info = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/info.png")
+	img_info = img_info.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_info = ImageTk.PhotoImage(img_info)
+
+	global img_help
+	img_help = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/help.png")
+	img_help = img_help.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_help = ImageTk.PhotoImage(img_help)
+
+	global img_mail
+	img_mail = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/mail.png")
+	img_mail = img_mail.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_mail = ImageTk.PhotoImage(img_mail)
+
+	global img_sort
+	img_sort = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/sort.png")
+	img_sort = img_sort.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_sort = ImageTk.PhotoImage(img_sort)
+
+	global img_delete
+	img_delete = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/delete.png")  
+	img_delete = img_delete.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_delete = ImageTk.PhotoImage(img_delete)
+
+	global img_reset
+	img_reset = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/reset.png")  
+	img_reset = img_reset.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_reset = ImageTk.PhotoImage(img_reset)
+
+	global img_edit
+	img_edit = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/edit.png")  
+	img_edit = img_edit.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_edit = ImageTk.PhotoImage(img_edit)
+
+	global img_choose_file
+	img_choose_file = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/choose_file.png")  
+	img_choose_file = img_choose_file.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_choose_file = ImageTk.PhotoImage(img_choose_file)
+
+	global img_choose_backup
+	img_choose_backup = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/choose_backup.png")  
+	img_choose_backup = img_choose_backup.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_choose_backup = ImageTk.PhotoImage(img_choose_backup)
+
+	global img_export
+	img_export = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/export.png")  
+	img_export = img_export.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_export = ImageTk.PhotoImage(img_export)
+
+	global img_backup
+	img_backup = Image.open("/Users/sanjaymarison/Library/Mobile Documents/com~apple~CloudDocs/Audit future/audit future/Resources/backup.png")  
+	img_backup = img_backup.resize((25, 25), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img_backup = ImageTk.PhotoImage(img_backup)
+
+
 	def backup_():
 		file_name = filedialog.askopenfilename(initialdir=str(backup_path),title="Select backup file")
 		if file_name != "":
@@ -75,71 +148,71 @@ def viewer_api(date1="Date",name1="Name",category1="Category",payement1="Payemen
 			amount_viewer_search = "Amount"
 
 
-		sqlite_statement = "SELECT * FROM TALLY WHERE "
+		sqlite_statement = "SELECT rowid,* FROM TALLY WHERE "
 		sqlite_list = []
 
 		if date_viewer_search != "Date":
-		    if sqlite_statement == "SELECT * FROM TALLY WHERE ":
+		    if sqlite_statement == "SELECT rowid,* FROM TALLY WHERE ":
 		        sqlite_statement = sqlite_statement + "date=?"
 		    else:
 		        sqlite_statement = sqlite_statement + " AND date=?"
 		    sqlite_list.append(date_viewer_search)
 
 		if name_viewer_search != "Name":
-		    if sqlite_statement == "SELECT * FROM TALLY WHERE ":
+		    if sqlite_statement == "SELECT rowid,* FROM TALLY WHERE ":
 		        sqlite_statement = sqlite_statement + "name=?"
 		    else:
 		        sqlite_statement = sqlite_statement + " AND name=?"
 		    sqlite_list.append(name_viewer_search)
 
 		if amount_viewer_search != "Amount":
-		    if sqlite_statement == "SELECT * FROM TALLY WHERE ":
+		    if sqlite_statement == "SELECT rowid,* FROM TALLY WHERE ":
 		        sqlite_statement = sqlite_statement + "amount=?"
 		    else:
 		        sqlite_statement = sqlite_statement + " AND amount=?"
 		    sqlite_list.append(amount_viewer_search)
 
 		if category_viewer_search != "Category":
-		    if sqlite_statement == "SELECT * FROM TALLY WHERE ":
+		    if sqlite_statement == "SELECT rowid,* FROM TALLY WHERE ":
 		        sqlite_statement = sqlite_statement + "category=?"
 		    else:
 		        sqlite_statement = sqlite_statement + " AND category=?"
 		    sqlite_list.append(category_viewer_search)
 
 		if payement_viewer_search != "Payement":
-		    if sqlite_statement == "SELECT * FROM TALLY WHERE ":
+		    if sqlite_statement == "SELECT rowid,* FROM TALLY WHERE ":
 		        sqlite_statement = sqlite_statement + "payement=?"
 		    else:
 		        sqlite_statement = sqlite_statement + " AND payement=?"
 		    sqlite_list.append(payement_viewer_search)
 
 		if site_viewer_search != "Site":
-		    if sqlite_statement == "SELECT * FROM TALLY WHERE ":
+		    if sqlite_statement == "SELECT rowid,* FROM TALLY WHERE ":
 		        sqlite_statement = sqlite_statement + "site=?"
 		    else:
 		        sqlite_statement = sqlite_statement + " AND site=?"
 		    sqlite_list.append(site_viewer_search)
 
 		tuple(sqlite_list)
-		if sqlite_statement == "SELECT * FROM TALLY WHERE ":
-			sqlite_statement = "SELECT * FROM TALLY"
+		if sqlite_statement == "SELECT rowid,* FROM TALLY WHERE ":
+			sqlite_statement = "SELECT rowid,* FROM TALLY"
 		c.execute(sqlite_statement,sqlite_list)
 		column_s_no = 0
 		total_amount = []
-		my_tree.delete(*my_tree.get_children())
+		data_table.delete(*data_table.get_children())
 		for row in c.fetchall():
 			column_s_no += 1
-			my_tree.insert(parent='',index='end',iid=(column_s_no-1),text=column_s_no,values=(row[0],row[1],row[3],row[4],row[5],row[2]))
-			total_amount.append(row[2])
-		label_total.config(text=str("Total amount:" + str(sum(total_amount))))
+			data_table.insert(parent='',index='end',iid=row[0],text=row[0],values=(row[1],row[2],row[4],row[5],row[6],format_num(row[3])))
+			total_amount.append(row[3])
+		label_total.config(text=str("Total amount:" + format_num(str(sum(total_amount)))))
 
 	conn = sqlite3.connect(file)
 	c = conn.cursor()
-	c.execute("SELECT * FROM TALLY")
+	c.execute("SELECT rowid,* FROM TALLY")
 
-	window_viewer = Tk()
+	window_viewer = Toplevel()
 	window_viewer.title("Audit viewer")
-	window_viewer.state("zoomed")
+	window_viewer.state("normal")
 	window_viewer.resizable(False,False)
 
 	status_viewer = Label(window_viewer)
@@ -170,19 +243,17 @@ def viewer_api(date1="Date",name1="Name",category1="Category",payement1="Payemen
 	site_viewer.insert(0,site1)
 
 	def sort_data_and_destroy(file):
-		window_viewer.destroy()
 		sort_data(file)
-		viewer_api()
+		search_api()
 
 
 	def reset():
 		def reset_confirm():
-			window_viewer.destroy()
-			conn = sqlite3.connect(DEFAULT_PATH)
+			conn = sqlite3.connect(file)
 			c = conn.cursor()
 			c.execute("DELETE FROM TALLY")
 			conn.commit()
-			viewer_api()
+			search_api()
 
 
 		response = messagebox.askyesno("RESET","Are you sure you want to reset? (YOU CANT UNDO THIS)")
@@ -191,111 +262,149 @@ def viewer_api(date1="Date",name1="Name",category1="Category",payement1="Payemen
 		elif response== 0:
 			pass
 
+	
+	search_viewer = Button(status_viewer,image=img_search,command=search_api,borderwidth=0,bd=0)
+	search_viewer.grid(row=0,column=6)
+
+	frame_button = Frame(status_viewer)
+	frame_button.grid(row=1,column=4,columnspan=3,ipady=0)
 
 
-	search_viewer = Button(status_viewer,text="Search",width=20,command=search_api)
-	search_viewer.grid(row=1,column=2,columnspan=2,sticky=W+E)
+	'''
+	style for data_table unofficial
+		style = ttk.Style()
+		style.theme_use('clam')
+		style.configure('Treeview',
+	88464099842  271158		background='#D3D3D3',
+			foreground='black',
+			rowheight=50,
+			row=20,
+			fieldbackground='#D3D3D3')
+		style.map('Treeview',
+			background=[('selected','blue')])
+	'''
 
-	my_tree = ttk.Treeview(window_viewer,selectmode='browse')
+	data_table = ttk.Treeview(window_viewer,selectmode='browse')
 
 	#defining columns
-	my_tree['columns'] = ("Date","Name","Category","Payement","Site","Amount")
+	data_table['columns'] = ("Date","Name","Category","Payement","Site","Amount")
 
 
 	#format our columns
-	my_tree.column("#0",width=120,minwidth=25)
-	my_tree.column("Date",anchor=W,width=120,minwidth=25)
-	my_tree.column("Name",anchor=W,width=120,minwidth=25)
-	my_tree.column("Category",anchor=W,width=120,minwidth=25)
-	my_tree.column("Payement",anchor=W,width=120,minwidth=25)
-	my_tree.column("Site",anchor=W,width=120,minwidth=25)
-	my_tree.column("Amount",anchor=W,width=120,minwidth=25)
+	data_table.column("#0",width=120,minwidth=25)
+	data_table.column("Date",anchor=W,width=120,minwidth=25)
+	data_table.column("Name",anchor=W,width=120,minwidth=25)
+	data_table.column("Category",anchor=W,width=120,minwidth=25)
+	data_table.column("Payement",anchor=W,width=120,minwidth=25)
+	data_table.column("Site",anchor=W,width=120,minwidth=25)
+	data_table.column("Amount",anchor=W,width=120,minwidth=25)
 
 	#create headings
-	my_tree.heading("#0",text="s.no",anchor=W)
-	my_tree.heading("Date",text="Date",anchor=W)
-	my_tree.heading("Name",text="Name",anchor=W)
-	my_tree.heading("Category",text="Category",anchor=W)
-	my_tree.heading("Payement",text="Payement",anchor=W)
-	my_tree.heading("Site",text="Site",anchor=W)
-	my_tree.heading("Amount",text="Amount",anchor=W)
+	data_table.heading("#0",text="rowid",anchor=W)
+	data_table.heading("Date",text="Date",anchor=W)
+	data_table.heading("Name",text="Name",anchor=W)
+	data_table.heading("Category",text="Category",anchor=W)
+	data_table.heading("Payement",text="Payement",anchor=W)
+	data_table.heading("Site",text="Site",anchor=W)
+	data_table.heading("Amount",text="Amount",anchor=W)
 
 
-	vertical_scrollbar = ttk.Scrollbar(window_viewer,orient="vertical",command=my_tree.yview)
+	vertical_scrollbar = ttk.Scrollbar(window_viewer,orient="vertical",command=data_table.yview)
 	vertical_scrollbar.grid(row=2,column=0,sticky=N+S+E)
 
-	my_tree.configure(yscrollcommand=vertical_scrollbar.set)
+	data_table.configure(yscrollcommand=vertical_scrollbar.set)
 
 	def selected_option_delete():
 		try:
-			x =  my_tree.selection()
+			x =  data_table.selection()
 			for selected in x:
-				my_tree.delete(selected)
+				data_table.delete(selected)
 				print(selected)
 				conn = sqlite3.connect(file)
 				c = conn.cursor()
 				selected_ = int(selected) + 1
 				c.execute("DELETE FROM TALLY WHERE rowid=?",(selected_,))
 			conn.commit()
+			refresh()
+			search_api()
 		except:
 			print("User is clicking delete without choosing an option")
 
 
 	def selected_option_edit():
+		def edit_(num_):
+			date = str(date_edit.get())
+			name = str(name_edit.get())
+			amount = str(amount_edit.get())
+			site = str(site_edit.get())
+			category = str(category_edit.get())
+			option = str(options.get())
+			num = str(num_)
+			statement = f'UPDATE TALLY SET name = "{name}" , date = "{date}" , amount = "{amount}" , site = "{site}" , category = "{category}" , payement = "{option}" WHERE rowid = "{num}"'
+			print(statement)
+			c.execute(statement)
+			conn.commit()
+			search_api()
+			ask_new_values.destroy()
+		x =  data_table.selection()
+		print(x)
+
+		num = x[0]
+		conn = sqlite3.connect(file)
+		c = conn.cursor()
+		num_ = num
+		c.execute("SELECT * FROM TALLY WHERE rowid=?",(num_,))
+		
+		for row in c.fetchall():
+			print(row)
+			date = row[0]
+			name = row[1]
+			amount = row[2]
+			site = row[5]
+			category = row[3]
+			if str(row[4]) == "Credited":
+				payement = 0
+			else:
+				payement = 1
 		#chooses wrong option some times might be buggy 
 		#Not recommended for use
-		def edit_():
-			pass
 
-		ask_new_values = Tk()
+		ask_new_values = Toplevel()
 		ask_new_values.title("Edit")
 		ask_new_values.resizable(False,False)
+
 		label_say = Label(ask_new_values,text="Enter new values")
 		label_say.pack()
+
 		date_edit = Entry(ask_new_values,font=font)
 		date_edit.pack()
+		date_edit.insert(0,date)
+
 		name_edit = Entry(ask_new_values,font=font)
 		name_edit.pack()
+		name_edit.insert(0,name)
+
 		amount_edit = Entry(ask_new_values,font=font)
 		amount_edit.pack()
+		amount_edit.insert(0,amount)
+
 		site_edit = Entry(ask_new_values,font=font)
 		site_edit.pack()
+		site_edit.insert(0,site)
+
 		options = StringVar()
 		category_edit = Entry(ask_new_values,font=font)
 		category_edit.pack()
+		category_edit.insert(0,category)
+
 		payement_edit = ttk.Combobox(ask_new_values,textvariable=options)
 		payement_edit['values'] = ("Credited","Debited","select")
 		payement_edit.config(width=15)
-		
-
-		x =  my_tree.selection()
-		print(x)
-
-		for num in x:
-			conn = sqlite3.connect(file)
-			c = conn.cursor()
-			print(int(num))
-			num_ = int(num)+1
-			print(num_)
-			c.execute("SELECT * FROM TALLY WHERE rowid=?",(num_,))
-			print(c.fetchall())
-			for row in c.fetchall():
-				print(row)
-				date_edit.insert(0,row[0])
-				name_edit.insert(0,row[1])
-				amount_edit.insert(0,row[2])
-				site_edit.insert(0,row[5])
-				category_edit.insert(0,row[3])
-				if str(row[4]) == "Credited":
-					payement_edit.current(0)
-				else:
-					payement_edit.current(1)
-				print(row[4])
-
 		payement_edit.pack()
-		button_edit = Button(ask_new_values,text="Edit",command=edit_)
+		payement_edit.current(payement)
+
+		button_edit = Button(ask_new_values,text="Edit",command=lambda: edit_(num_))
 		button_edit.pack()
-		ask_new_values.mainloop()
 
 	#add data
 	column_s_no = 0
@@ -303,48 +412,48 @@ def viewer_api(date1="Date",name1="Name",category1="Category",payement1="Payemen
 
 	for row in c.fetchall():
 		column_s_no += 1
-		my_tree.insert(parent='',index='end',iid=(column_s_no-1),text=column_s_no,values=(row[0],row[1],row[3],row[4],row[5],row[2]))
-		total_amount.append(row[2])
+		data_table.insert(parent='',index='end',iid=row[0],text=row[0],values=(row[1],row[2],row[4],row[5],row[6],format_num(row[3])))
+		total_amount.append(row[3])
 
-	my_tree.grid(row=2,column=0,pady=40,sticky=N+S+W+E,ipady=2)
+	data_table.grid(row=2,column=0,sticky=N+S+W+E)
 	
-	label_total = Label(window_viewer,font=font,text=str("Total amount:" + str(sum(total_amount))))
-	label_total.grid(row=3,column=0)
+	label_total = Label(window_viewer,font=font,text=str("Total amount:" + format_num(str(sum(total_amount)))))
+	label_total.grid(row=3,column=0,pady=20)
 
-	data_tools = LabelFrame(window_viewer,border=5)
-	data_tools.grid(row=4,column=0,columnspan=1)
+	status_viewer = LabelFrame(window_viewer,border=5)
+	status_viewer.grid(row=4,column=0,columnspan=1)
 
-	reset_button = Button(data_tools,width=b_wd,text="RESET",command=reset)
+	reset_button = Button(frame_button,width=22,image=img_reset,command=reset)
 	reset_button.grid(row=0,column=0,pady=10)
-	reset_button.config(bg=button_bg,fg=button_fg,font=font)
 
-	sort_data_button = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Sort data",command=lambda: sort_data_and_destroy(file))
+	sort_data_button = Button(frame_button,image=img_sort,command=lambda: sort_data_and_destroy(file))
 	sort_data_button.grid(row=0,column=1,sticky=W+E)
 
-	change_file = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Change file",command=change_file_)
+	change_file = Button(frame_button,image=img_choose_file,command=change_file_)
 	change_file.grid(row=0,column=2,sticky=W+E)
 
-	open_backup = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Open Backup",command=backup_)
+	open_backup = Button(frame_button,image=img_choose_backup,command=backup_)
 	open_backup.grid(row=0,column=3,sticky=W+E)
 
-	delete_record = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Delete",command=selected_option_delete)
+	delete_record = Button(frame_button,image=img_delete,command=selected_option_delete)
 	delete_record.grid(row=0,column=4,sticky=W+E)
 
-	edit_record = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Edit",command=selected_option_edit,state=DISABLED)
+	edit_record = Button(frame_button,image=img_edit,command=selected_option_edit)
 	edit_record.grid(row=0,column=5,sticky=W+E)
 
-	export_record = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Export record",command=export_window)
+	export_record = Button(frame_button,image=img_export,command=export_window)
 	export_record.grid(row=0,column=6,sticky=W+E)
 
-	backup_record = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Backup record",command=create_backup)
+	backup_record = Button(frame_button,image=img_backup,command=create_backup)
 	backup_record.grid(row=0,column=7,sticky=W+E)
 
-	send_record_ = Button(data_tools,bg=button_bg,fg=button_fg,font=font,text="Send record",command=send_record)
+	send_record_ = Button(frame_button,image=img_mail,command=send_record)
 	send_record_.grid(row=0,column=8,sticky=W+E)
 
 
-
+	#to run the viewer's search from the main screen if input is provided
 	if z == 1:
 		search_api()
 
-	window_viewer.mainloop()
+
+
